b42fb9d90272aad6feea01040b3e7f9b4887a4af
Ivan A <ivanhub@webera.pro>
2017-05-09 20:09:03 +0400

    5.9 NEW FOOTER

--- frontend/views/layouts/footer.php
+++ frontend/views/layouts/footer.php
@@ -8,15 +8,16 @@ use yii\helpers\Html;
 <div class="row">
 <div class="col-md-2  col-sm-2 col-xs-2 foocol"></div>
 
-<div class="col-md-3   col-sm-3 col-xs-5">
- <p><a href="/driving-school">Автомотошкола</a></p>   
- <p><a href="/driving-school/A">Категория А</a></p>
-  <p><a href="/driving-school/B">Категория B</a></p>
-  <p style="padding-top:0px"><b><a href="/self-propelled-machinery">Самоходная техника</a></b></p>
-  <p style="padding-top:0px"><b><a href="/navigation">Судовождение</a></b></p>   
-  <p style="padding-top:0px"><b><a href="/safety-driving">Защитное вождение</a></b></p>   
-  <p style="padding-top:0px"><b><a href="/distance-education">Дистанционное обучение</a></b></p>   
-
+<div class="col-md-3   col-sm-3 col-xs-5 col1">
+<i class="footer__vline footer__vline_1"></i>
+ <ul class="footer-links">
+ <li><a href="/driving-school"><i class="footer__hline footer__hline_1"></i>Автомотошкола</a></li>
+ <li class="indent10"><i class="footer__hline footer__hline_1"></i><a href="/driving-school/A">Категория А</a></li>
+  <li class="indent10"><a href="/driving-school/B">Категория B</a></li>
+  <li style="font-size:14.5px;padding-top:0px"><b><a href="/self-propelled-machinery">Самоходная техника</a></b></li>
+  <li style="font-size:14.5px;padding-top:0px"><b><a href="/navigation">Судовождение</a></b></li>   
+  <li style="font-size:14.5px;padding-top:0px"><b><a href="/safety-driving">Защитное вождение</a></b></li>   
+</ul>
  <!-- <p><a href="/postupayushchim">Запись на обучение</a></p>
  <p><a href="/raspisanie">Расписание занятий</a></p>
  <p><a href="/our-advantages">Наши преимущества</a></p>
@@ -27,51 +28,68 @@ use yii\helpers\Html;
   <div class="col-xs-2 foocol hidden-sm visible-xs"></div>
 
 <div class="col-md-3 col-sm-3  col-xs-5 col4">
+<i class="footer__vline footer__vline_2"></i>
 
 <?php if (hm('site/dopuslugi')) {
   ?>
-<p><a href="#">Дополнительные услуги</a></p>   
- <p><a data-toggle="collapse" href="#specialsignals">Курс "Спецсигналы"</a></p>
- <p><a data-toggle="collapse" href="#DOPOG">Курс "ДОПОГ"</a></p>
- <p><a data-toggle="collapse" href="#instructor_training">Обучение инструкторов</a></p>
- <p><a data-toggle="collapse" href="#Arenda_avtodroma">Аренда автодрома</a></p>
- <p><a data-toggle="collapse" href="#consultation">Занятия с инструктором</a></p>
-
+   <ul class="footer-links">
+<li><a href="#">Дополнительные услуги</a></li>
+ <li><i class="footer__hline"></i><a data-toggle="collapse" href="#specialsignals">Курс "Спецсигналы"</a></li>
+ <li><i class="footer__hline"></i><a data-toggle="collapse" href="#DOPOG">Курс "ДОПОГ"</a></li>
+ <li><i class="footer__hline"></i><a data-toggle="collapse" href="#instructor_training">Обучение инструкторов</a></li>
+ <li><i class="footer__hline"></i><a data-toggle="collapse" href="#Arenda_avtodroma">Аренда автодрома</a></li>
+ <li><i class="footer__hline"></i><a data-toggle="collapse" href="#consultation">Занятия с инструктором</a></li>
+</ul>
   <?php } else { ?>
-<p><a href="/additional-services">Дополнительные услуги</a></p>   
- <p><a href="/additional-services#specialsignals">Курс "Спецсигналы"</a></p>
- <p><a href="/additional-services#DOPOG">Курс "ДОПОГ"</a></p>
- <p><a href="/additional-services#instructor_training">Обучение инструкторов</a></p>
- <p><a href="/additional-services#Arenda_avtodroma">Аренда автодрома</a></p>
- <p><a href="/additional-services#consultation">Занятия с инструктором</a></p>
-
+   <ul class="footer-links footer-links_right">
+<li><a href="/additional-services">Дополнительные услуги</a></li>
+ <li><i class="footer__hline"></i><a href="/additional-services#specialsignals">Курс "Спецсигналы"</a></li>
+ <li><i class="footer__hline"></i><a href="/additional-services#DOPOG">Курс "ДОПОГ"</a></li>
+ <li><i class="footer__hline"></i><a href="/additional-services#instructor_training">Обучение инструкторов</a></li>
+ <li><i class="footer__hline"></i><a href="/additional-services#Arenda_avtodroma">Аренда автодрома</a></li>
+ <li><i class="footer__hline"></i><a href="/additional-services#consultation">Занятия с инструктором</a></li>
+</ul>
 <?php } ?>
 
 </div>
-<div class="col-md-3  col-sm-3 col-xs-12 my"><div class="vk"><BR/>
-<p><?php    echo Html::a(
-    'Запись на занятия',
-  ['#'],
-  [ 'data-toggle' => 'modal',
-    'data-target' => '#idmodal',
-    'data-which' => '1',
-  ]
-    ); ?></p> 
+<div class="col-md-3  col-sm-3 col-xs-12 my">
 
-  <!--  <div class="wrapperb">
-    <p class="innerb">
-        <a href="/" class="buttonn">
+<div class="footer-phone">
+<p class="phone">
+<a href="tel:8846255-69-99"><i class="footer-phone__pic"></i> <span>+7 (846) 255-69-99</span></a></p>
+<p class="footer-phone__p">пн-чт с 8.00 до 17.00</p>
+<p class="footer-phone__p">пт с 8.00 до 16.00</p>
+</div>
+<div class="vk">
+<?php    echo Html::a(
+   '<div class="wrapperb">
+        <p class="buttonn">
             <span class="borderr"></span>
             <span class="topp"></span>
             <span class="rightt"></span>
             <span class="bottomm"></span>
             <span class="leftt"></span>
-            <span class="textt">Запись на обучение</span>
-        </a>
-    </p>
-</div>http://codepen.io/anon/pen/mWJmmm?editors=1100-->
-<p style="margin-left:-10px">  <a target="blank" href="https://vk.com/rosavtoakademia" class="icon icon-xxs icon-circle icon-white hi-icon"><i class=" fa fa-vk"></i></a><a href="https://vk.com/rosavtoakademia" target="blank" class="myvk">Мы Вконтакте</a></p><BR/>
-<!--<div class="hi-icon-effect-8 "> -->
+            <span class="textt">Запись на занятия</span>
+        </p>
+</div>',
+  ['#'],
+  [
+  'data-toggle' => 'modal',
+  'data-target' => '#idmodal',
+  'data-which' => '1',
+  ]
+    ); ?> 
+<BR/>
+
+  <!--http://codepen.io/anon/pen/mWJmmm?editors=1100-->
+<!-- <p style="margin-left:85px;text-align:center"><a href="https://vk.com/rosavtoakademia" target="blank" class="myvk"><i class="icon icon-xxs icon-circle icon-white hi-icon"><i class=" fa fa-vk"></i></i>Мы Вконтакте</a></p>
+-->
+<!--<div class="hi-icon-effect-8 ">              <i class="my-vk"></i>-->
+
+<!--<p style="margin-top:15px"><a href="https://vk.com/rosavtoakademia" target="blank" class="myvk"><span>Мы Вконтакте &nbsp;</span> <i class="icon-button vk1"> <i class="fa fa-vk"><span></span></i></i></a></p>-->
+
+<p class="footer__myvkontakte  text-right"><a href="https://vk.com/rosavtoakademia" target="blank" class="myvk"><span>Мы Вконтакте </span> <i class="social-btns"><i class="btn vk" href="#"><i class="fa fa-vk"></i></i></i></a></p>
+
 
 </div>
 </div>
@@ -93,10 +111,15 @@ use yii\helpers\Html;
 <!-- <div id="w00" class="col-xs-11 col-sm-3 alert alert-custom  fadeInDown" role="alert" data-notify="container" data-notify-position="top-center" style="display: inline-block; margin: 0px auto; position: fixed; transition: all 0.5s ease-in-out; z-index: 1031; top: 20px; left: 0px; right: 0px; animation-iteration-count: 1;"><button type="button" class="close" data-notify="dismiss"><span aria-hidden="true">×</span></button><span data-notify="icon" class="fa fa-envelope"></span><span data-notify="title">Сообщение отправлено!</span><hr class="kv-alert-separator"><span data-notify="message">Спасибо, что обратились к Нам. <br/>Мы ответим Вам в ближайшее время.</span><a href="#" data-notify="url" target="_blank"></a></div> -->
 
 <?php 
+
+
+
 $this->registerJs(<<<JS
 var div = document.createElement('div');
 div.className = "greenlinebot";
+
   document.getElementsByClassName("navbar-header")[0].appendChild(div);
+
   $('form').on('beforeSubmit', function(e) {
     var form = $(this);
     var formData = form.serialize();
@@ -111,8 +134,10 @@ div.className = "greenlinebot";
 //                                  $('#idmodal').modal('toggle');
 //                      setTimeout(function(){        $("#w00").removeClass('animated');
 //                        }, 3000);
+
   //console.log(result);
 //if (result == 'true') {
+
 var modalContainer = $('#idmodal');
 var modalBody = modalContainer.find('.modal-body');
 var insidemodalBody = modalContainer.find('#contact-form');
@@ -121,12 +146,17 @@ $('.sel').html("<div class='alert alert-success'>");
 $('.sel > .alert-success').append("<strong>Спасибо! Заявка отправлена.</strong>");
 $('.sel > .alert-success').append('</div>');
 //$('.sel').css('visibility', 'visible');
+
 setTimeout(function() { 
 $("#idmodal").modal('hide');
 //$('#w00').css('visibility', 'hidden');
 }, 4000);
+
 //}
+
 //else { modalBody.html(result).hide().fadeIn();                }
+
+
         },
         error: function () {
             alert("Something went wrong");
@@ -135,6 +165,8 @@ $("#idmodal").modal('hide');
 }).on('submit', function(e){
     e.preventDefault();
 });
+
+
 /*
 "use strict";
 function start5() {
@@ -146,6 +178,7 @@ function start5() {
         });
     }
 }*/
+
 JS
 , yii\web\View::POS_READY, 'contact-form'); ?>
 
@@ -153,6 +186,7 @@ JS
 <footer class="footer">
     <div class="container">
         <p class="pull-left"> My Company </p>
+
         <p class="pull-right"></p>
     </div>
 </footer> -->
