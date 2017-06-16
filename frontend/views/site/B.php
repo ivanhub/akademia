68dc1ba3dbb17b60bb85deadf59aa21e6b964a45
Ivan A <ivanhub@webera.pro>
2017-06-16 16:31:20 +0400

    mobile

--- frontend/views/site/B.php
+++ frontend/views/site/B.php
@@ -562,7 +562,7 @@ echo'
 <div class="well">
 <div class="row">
 
-<div class="col-md-8 col-xs-6">
+<div class="col-md-8 col-xs-12">
 
 
 <div class="row">
@@ -618,7 +618,7 @@ echo'
 
 </div> <!--col md-8 -->
 
-<div class="col-md-4 sidehere">
+<div class="col-md-4 col-xs-12 sidehere">
 
 </div>
 
